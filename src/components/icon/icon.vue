f932a088efdbb77beef4e7ff9dbe90c127fb6d0c
huangqifei <1615719324@qq.com>
2019-10-26 10:08:31 +0800

    修改icon样式

--- src/components/icon/icon.vue
+++ src/components/icon/icon.vue
@@ -1,42 +1,42 @@
 <template>
-	<svg 
-		class="v-icon" 
-		@click="clickHandle"
-		aria-hidden="true" 
-		:style="{fill: color, width: `${width}em`, height: `${height}em`}">
-		<use :xlink:href="`#v-${name}`"></use>
-	</svg>
+  <svg 
+    class="v-icon" 
+    @click="clickHandle"
+    aria-hidden="true" 
+    :style="{fill: color, width: `${width}em`, height: `${height}em`}">
+    <use :xlink:href="`#v-${name}`"></use>
+  </svg>
 </template>
 <script>
 import './svg.js'
 
 export default {
-	name: 'v-icon',
-	props: {
-		name: {
-			type: String
-		},
-		color: {
-			type: String
-		},
-		width: {
-			type: Number,
-			default: 1
-		},
-		height: {
-			type: Number,   
-			default: 1
-		}
-	},
-	methods: {
-		clickHandle(){
-			this.$emit('click')
-		}
-	}
+  name: 'v-icon',
+  props: {
+    name: {
+      type: String
+    },
+    color: {
+      type: String
+    },
+    width: {
+      type: Number,
+      default: 1
+    },
+    height: {
+      type: Number,   
+      default: 1
+    }
+  },
+  methods: {
+    clickHandle(){
+      this.$emit('click')
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
-	.v-icon{
-		overflow: hidden;
-	}
+  .v-icon{
+    overflow: hidden;
+  }
 </style>
